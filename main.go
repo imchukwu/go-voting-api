3bbb8737c702cc2c7d2a033971f01f11f1abf0ff
Ikechukwu Chukwu <imchukwu@gmail.com>
2025-05-27 22:01:23 +0100

    updated cors

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-    "go-voting-api/config"
-    "go-voting-api/routes"
-    "github.com/gin-gonic/gin"
+	"go-voting-api/config"
+	"go-voting-api/routes"
+
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    config.ConnectDatabase()
-    routes.RegisterRoutes(r)
+	// Enable CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // Replace * with specific domains in production
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
-    r.Run() // default is :8080
+	config.ConnectDatabase()
+	routes.RegisterRoutes(r)
+
+	r.Run() // default is :8080
 }
